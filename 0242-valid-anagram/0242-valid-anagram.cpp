b43cddd7cfe48cc258e227230f0f7c507cdbd4ca
techlism <105585386+techlism@users.noreply.github.com>
2022-11-23 09:34:40 +0530

    Time: 29 ms (30.09%), Space: 9.8 MB (13.03%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        vector<int>ans1;
+        vector<int>ans2;
+        int len=max(s.length(),t.length());
+        for(int i=0;i<len;i++){
+            ans1.push_back(s[i]);
+            ans2.push_back(t[i]);
+        }
+        if(ans1.size()!=ans2.size()){
+            return false;
+        }
+        sort(ans1.begin(),ans1.end());
+        sort(ans2.begin(),ans2.end());
+        for(int i=0;i<ans2.size();i++){
+            if(ans1[i]!=ans2[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+};
