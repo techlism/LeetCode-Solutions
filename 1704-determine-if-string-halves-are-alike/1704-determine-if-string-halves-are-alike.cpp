5ebdaddad18e06651a49771331652f90791eb9c4
techlism <105585386+techlism@users.noreply.github.com>
2022-12-01 07:45:11 +0530

    Time: 6 ms (58.35%), Space: 6.6 MB (56.35%) - LeetHub

---
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool halvesAreAlike(string s) {
+        int countv1=0;
+        int countv2=0;
+        for(int i=0;i<s.length();i++){
+            if(i<=s.length()/2 -1 && (s[i]=='a'||s[i]=='e'||  s[i]=='i' || s[i]=='o' || s[i]=='u'||s[i]=='A'||s[i]=='E'||  s[i]=='I' || s[i]=='O' || s[i]=='U' )){
+                countv1++;
+            }
+            if(i>=s.length()/2 && (s[i]=='a'||s[i]=='e'||  s[i]=='i' || s[i]=='o' || s[i]=='u'||s[i]=='A'||s[i]=='E'||  s[i]=='I' || s[i]=='O' || s[i]=='U' )){
+                countv2++;
+            }
+        }
+        if(countv1 == countv2){
+            return true;
+        }
+        return false;
+    }
+};
