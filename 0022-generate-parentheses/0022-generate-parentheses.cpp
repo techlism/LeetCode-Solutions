7368708a1b61c9afc024ccbd79edcdad6408d668
techlism <105585386+techlism@users.noreply.github.com>
2022-12-09 01:03:47 +0530

    Update 0022-generate-parentheses.cpp

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,13 +1,17 @@
 class Solution {
+    //solved by Understanding from a Tutorial
 private:
     void findAllParenthesis(string current,int open,int close,vector<string>&result,int n){
+        //When length of current string reaches to 2 * n push in to Answer
         if(current.length()==2*n){
             result.push_back(current);
             return;
         }
+        //If Opening parenthesis are less than n then we can generate more parenthesis.
         if(open<n){
             findAllParenthesis(current+'(',open+1,close,result,n);
         }
+        //if closing parenthesis are less than opening ones then we can generate more closing parenthesis.
         if(close<open){
             findAllParenthesis(current+')',open,close+1,result,n);
         }
