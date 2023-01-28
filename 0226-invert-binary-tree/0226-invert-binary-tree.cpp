b75ed5b16a0fcee17ebf99d79ab41942efe47715
techlism <105585386+techlism@users.noreply.github.com>
2023-01-28 14:57:43 +0530

    Update 0226-invert-binary-tree.cpp

--- 0226-invert-binary-tree/0226-invert-binary-tree.cpp
+++ 0226-invert-binary-tree/0226-invert-binary-tree.cpp
@@ -9,6 +9,9 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+//Logic - I have just used a post order traversal and swapped the nodes (apparently I was not sure that node swapping is also possible).
+//Why postorder?
+//Because in postorder you will have both the left and right nodes before swapping.
 class Solution {
 public:
     TreeNode* invertTree(TreeNode* root) {
