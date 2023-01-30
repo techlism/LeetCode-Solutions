d82d308d5c37ad3d0349372e25ec6a6f47a524be
techlism <105585386+techlism@users.noreply.github.com>
2023-01-30 21:00:34 +0530

    Time: 124 ms (43.08%), Space: 63.7 MB (14.21%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
@@ -16,29 +16,28 @@ public:
         queue<TreeNode *> q;
         q.push(root);
         while(!q.empty()){
-            vector<int>level;
-            int size=q.size();
-            for(int i=0;i<size;i++){
-                TreeNode * front = q.front();
-                q.pop();
-                if(front->left==nullptr && front->right==nullptr){
-                    level.push_back(front->val);
-                }
-                    if(front->left){
-                        q.push(front->left);
-                    }
-                    if(front->right){
-                        q.push(front->right);
-                    }
-                }
-            leaves.push_back(level);
-            }
+         vector<int>level;
+         int size=q.size();
+         for(int i=0;i<size;i++){
+          TreeNode * front = q.front();
+          q.pop();
+          if(front->left==nullptr && front->right==nullptr){
+            level.push_back(front->val);
+          }
+          if(front->left){
+            q.push(front->left);
+          }
+          if(front->right){
+            q.push(front->right);
+          }
+        }
+        leaves.push_back(level);
+        }
         int ans=0;
         int size=leaves.size();
         for(int i=0;i<leaves[size-1].size();i++){
             ans=ans+leaves[size-1][i];
         }
-        return ans;
-        
+        return ans; 
     }
 };
