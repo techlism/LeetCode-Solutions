45cccb80dd6d43696761ff13ddaf544bd402e5db
techlism <105585386+techlism@users.noreply.github.com>
2023-02-01 20:09:10 +0530

    Time: 265 ms (74.62%), Space: 71.2 MB (75.50%) - LeetHub

--- 2415-reverse-odd-levels-of-binary-tree/2415-reverse-odd-levels-of-binary-tree.cpp
+++ 2415-reverse-odd-levels-of-binary-tree/2415-reverse-odd-levels-of-binary-tree.cpp
@@ -12,8 +12,12 @@
 class Solution {
 private:
     void reverse(TreeNode* rootL, TreeNode* rootR, int level) {
-        if(rootL == NULL and rootR == NULL) return;
-        if(level % 2 == 0) swap(rootL->val, rootR->val);
+        if(rootL == nullptr and rootR == nullptr){
+               return;    
+        } 
+        if(level % 2 == 0) {
+            swap(rootL->val, rootR->val);
+        }
         reverse(rootL->left, rootR->right, level + 1);
         reverse(rootL->right, rootR->left, level + 1);
     }
