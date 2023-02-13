3489612dc176115e510aad8e69297ef761b9e935
techlism <105585386+techlism@users.noreply.github.com>
2023-02-13 06:59:56 +0530

    Time: 3 ms (66.30%), Space: 6 MB (63.61%) - LeetHub

--- 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.cpp
+++ 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.cpp
@@ -1,32 +1,35 @@
 class Solution {
 public:
     int countOdds(int low, int high) {
-        int count = 0;
-        if(low%2==0 && high%2==0){
-            while(low<high){
-                count++;
-                low=low+2;
-            }
-        }
-        else if(low%2!=0 && high%2!=0){
-            while(low<=high){
-                count++;
-                low=low+2;  
-            }
-        }
-        else if(low%2==0 && high%2!=0){
-            low=low+1;
-             while(low<=high){
-                count++;
-                low=low+2;  
-            }           
-        }
-        else{
-             while(low<high){
-                count++;
-                low=low+2;  
-            }            
-        }
-        return count;
+        // int count = 0;
+        // if(low%2==0 && high%2==0){
+        //     while(low<high){
+        //         count++;
+        //         low=low+2;
+        //     }
+        // }
+        // else if(low%2!=0 && high%2!=0){
+        //     while(low<=high){
+        //         count++;
+        //         low=low+2;  
+        //     }
+        // }
+        // else if(low%2==0 && high%2!=0){
+        //     low=low+1;
+        //      while(low<=high){
+        //         count++;
+        //         low=low+2;  
+        //     }           
+        // }
+        // else{
+        //      while(low<high){
+        //         count++;
+        //         low=low+2;  
+        //     }            
+        // }
+        // return count;
+        
+        //One liner
+        return ((high - low) >> 1) + ((low | high) & 1);        
     }
 };
