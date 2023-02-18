1ade4526166b58e645d7260bc6f7609d5a2f8a8f
techlism <105585386+techlism@users.noreply.github.com>
2023-02-18 22:50:04 +0530

    Time: 118 ms (70.49%), Space: 67.7 MB (62.48%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -2,16 +2,15 @@ class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
         // It's solved using a algorithm called Kardane's Algorithm
-        int maxm=INT_MIN;
-        int sum=0;
-        for(int i=0;i<nums.size();i++){
+        int maxm=nums[0];
+        int sum=0,n=nums.size();
+        for(int i=0;i<n;i++){
             sum=sum+nums[i];
             maxm=max(sum,maxm);
             if(sum<0){
                 sum=0;
             }
         }
-        return maxm;
-        
+        return maxm;        
     }
 };
