2639c9d0e10f2e7ae90f560553b32e7c49068aa2
techlism <105585386+techlism@users.noreply.github.com>
2023-02-20 08:23:24 +0530

    Time: 3 ms (90.12%), Space: 9.6 MB (77.52%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,15 +1,34 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        int count=0;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==target){
-                    return i;  
+        //O(N)
+       //  int count=0;
+       //  for(int i=0;i<nums.size();i++){
+       //      if(nums[i]==target){
+       //              return i;  
+       //      }
+       //      if(nums[i]<target){
+       //          count++;
+       //      }
+       //    }
+       // return count;
+       
+       //O(NLOGN)
+        
+        int s=0;
+        int e=nums.size();
+        int mid = s + (e-s)/2 ;
+        while(s<e){
+            if(nums[mid]>=target){
+                e=mid;
             }
-            if(nums[i]<target){
-                count++;
+            else{
+                s=mid+1;
             }
-          }
-       return count;
+            cout<<nums[mid]<<endl;
+            mid=s+(e-s)/2;
+        }
+        return s;
+        
 }
 };
