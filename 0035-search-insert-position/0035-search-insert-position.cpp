79677f279c8597878bc1a434cbb49489a801c9d5
techlism <105585386+techlism@users.noreply.github.com>
2023-02-20 08:00:09 +0530

    Time: 3 ms (90.12%), Space: 9.8 MB (31.02%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int count=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==target){
+                    return i;  
+            }
+            if(nums[i]<target){
+                count++;
+            }
+          }
+       return count;
+}
+};
