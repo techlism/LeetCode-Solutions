cd5c9618a3c78eaf1ccf8040d71b378098fb718a
techlism <105585386+techlism@users.noreply.github.com>
2023-02-21 09:09:03 +0530

    Time: 28 ms (67.14%), Space: 22.4 MB (62.74%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,20 +1,33 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        stack<int>s;
-        s.push(nums[0]);
-        int num=INT_MIN;
-        for(int i=1;i<nums.size();i++){
-            if(!s.empty() && s.top()==nums[i]){
-                num=s.top();
-                s.pop();
-            }
-            else{
-                if(num!=nums[i]){
-                    s.push(nums[i]);
-                }
-            }
+        //T.C. O(N) & S.C. O(N)
+        // stack<int>s;
+        // s.push(nums[0]);
+        // int num=INT_MIN;
+        // for(int i=1;i<nums.size();i++){
+        //     if(!s.empty() && s.top()==nums[i]){
+        //         num=s.top();
+        //         s.pop();
+        //     }
+        //     else{
+        //         if(num!=nums[i]){
+        //             s.push(nums[i]);
+        //         }
+        //     }
+        // }
+        // return s.top();
+        
+        //O(LOGN) & O(1) - Binary Search
+        
+        int s = 0, e = nums.size() - 1;
+        while(s < e){
+            int mid = s + (e-s)/2;
+            if((mid % 2 == 0 && nums[mid] == nums[mid + 1]) || (mid % 2 == 1 && nums[mid] == nums[mid - 1]))
+                s = mid + 1;
+            else
+                e = mid;
         }
-        return s.top();
-    }
+        return nums[s];
+    }     
 };
