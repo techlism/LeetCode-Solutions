bd97da4c17f60a38852983e09790fd4732db51a7
techlism <105585386+techlism@users.noreply.github.com>
2023-02-22 18:19:35 +0530

    Time: 7 ms (57.27%), Space: 8.1 MB (50.03%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -6,11 +6,14 @@ public:
     
         for(int i=0;i<s.length();i++){
             if(s[i]!=' '){
+                //If there's no space then do word formation
                 temp.push_back(s[i]);
             }
             else{
+                //If space found and our temporary string is not empty then push into stack
                 if(!temp.empty()){
                     st.push(temp);
+                    //The space we are pushing should be leading one
                     if(!st.empty())
                         st.push(" ");                   
                 }
@@ -20,8 +23,10 @@ public:
         st.push(temp);
         temp="";
         while(!st.empty()){
-            if(int(st.top()[0])==32 && temp.length()==0){
+            //Checking if any leading space is pushed into the stack if yes then skip them
+            if((st.top()[0])==' ' && temp.length()==0){
                 st.pop();
+                continue;
             }
             else{
                 temp.append(st.top());
