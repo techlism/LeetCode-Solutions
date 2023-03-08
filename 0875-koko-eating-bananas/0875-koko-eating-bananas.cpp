0246dd84e6d22384002a4e5c18979381fc4f3978
techlism <105585386+techlism@users.noreply.github.com>
2023-03-08 07:37:23 +0530

    Time: 41 ms (90.15%), Space: 19 MB (45.12%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -5,17 +5,21 @@ public:
         int e=*max_element(piles.begin(),piles.end());
         while(s<=e){
             int mid = s + (e-s)/2;
+            //size_t to avoid integer overflow
             size_t hours=0;
+            //checking for our each value in piles vector
             for(auto p: piles){
-                hours=hours+(p+mid-1)/mid;
+                hours += (p + mid - 1) / mid;
             }
+            //The time we chose was not sufficient
             if(hours>h){
                 s=mid+1;
             }
+            //there could be some smaller values
             else{
                 e=mid-1;
             }
         }
-        return s;
+        return e+1;
     }
 };
