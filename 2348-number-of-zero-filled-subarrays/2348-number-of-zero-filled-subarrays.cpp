92a32aeeb891981f794bd92652b3e0cfcf442e70
techlism <105585386+techlism@users.noreply.github.com>
2023-03-21 20:18:54 +0530

    Time: 182 ms (40.12%), Space: 107.6 MB (14.33%) - LeetHub

---
+++ 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.cpp
@@ -0,0 +1,31 @@
+class Solution {
+private:
+    long long sum(long long n){
+        return (n*(n+1))/2;
+    }
+public:
+    long long zeroFilledSubarray(vector<int>& nums) {
+        long long count = 0;
+        long long n = 0;
+        int  prev=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==0){
+                if(n==0){
+                    n++;
+                }
+                else if(i-prev!=1){
+                    count+=sum(n);
+                    n=1;
+                }
+                else if(i-prev==1){
+                    n++;
+                }
+                prev=i;
+            }
+        }
+        if(n){
+            count+=sum(n);
+        }
+        return count;
+    }
+};
