68aea768aec3c7365b469144b3f73b23c8a5ecc5
techlism <105585386+techlism@users.noreply.github.com>
2023-04-04 09:18:10 +0530

    Time: 64 ms (95.29%), Space: 43.1 MB (98.15%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.js
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.js
@@ -3,15 +3,16 @@
  * @return {number}
  */
 var myAtoi = function(s) {
-    if(Number.parseInt(s)){
+    const ans = Number.parseInt(s)
+    if(ans){
         // Less than INT_MIN(32 bit)
-        if(Number.parseInt(s) <= -2147483648 ) 
+        if(ans <= -2147483648 ) 
             return -2147483648 ;
         // Greater than INT_MAX(32 bit)
-        else if (Number.parseInt(s) >= 2147483647) 
+        else if (ans >= 2147483647) 
             return 2147483647;
         else
-            return Number.parseInt(s);
+            return ans;
     }
     else 
         return 0;
