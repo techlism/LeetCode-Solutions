154d882964e3aad532abb30817fcf6bd5cf598f6
techlism <105585386+techlism@users.noreply.github.com>
2023-04-16 12:19:25 +0530

    Time: 117 ms (23.95%), Space: 52.1 MB (65.89%) - LeetHub

---
+++ 2618-check-if-object-instance-of-class/2618-check-if-object-instance-of-class.js
@@ -0,0 +1,23 @@
+/**
+ * @param {Object} object
+ * @param {Function} classFunction
+ * @return {boolean}
+ */
+var checkIfInstanceOf = function(obj, classFunction) {
+ while(obj!=null)
+    {
+        if(obj.constructor === classFunction)
+        {
+            return true;
+        }
+
+        obj = Object.getPrototypeOf(obj);
+
+    }
+
+    return false;    
+};
+
+/**
+ * checkIfInstanceOf(new Date(), Date); // true
+ */
