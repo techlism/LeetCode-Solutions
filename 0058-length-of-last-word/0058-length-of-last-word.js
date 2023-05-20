b43f39ed8cce51941019e34c93abdfcf76435950
techlism <105585386+techlism@users.noreply.github.com>
2023-05-20 07:59:28 +0530

    Time: 64 ms (25.21%), Space: 43.8 MB (5.29%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.js
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -4,8 +4,6 @@
  */
 var lengthOfLastWord = function(s) {
     s=s.trim().split('').reverse().join('');
-    console.log(s.length);
-    if(s.length===1) return 1;
     let i=0;
     while(s[i]!=" " && i<s.length) i++;
     return i;
