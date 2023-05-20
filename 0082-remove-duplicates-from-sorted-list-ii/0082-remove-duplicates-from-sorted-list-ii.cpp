d85e2bc1b8da73d70120d4c1d069030c7a19a17d
techlism <105585386+techlism@users.noreply.github.com>
2023-05-20 14:38:37 +0530

    Time: 4 ms (87.65%), Space: 11.4 MB (17.99%) - LeetHub

--- 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.cpp
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.cpp
@@ -8,31 +8,72 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+// class Solution {
+// private:  
+//     void findDuplicates(ListNode * head , vector<int> & duplicates) {
+//         if(!head) return;
+//         while(head && head->next){
+//             if(head->val==head->next->val){
+//                 duplicates.push_back(head->val);
+//             }
+//             head=head->next;
+//         }
+//     }   
+// public:
+//     ListNode* deleteDuplicates(ListNode* head) {
+//         vector<int> duplicates;
+//         findDuplicates(head,duplicates);
+//         ListNode * ans = new ListNode(0);
+//         ListNode * start = ans;
+//         while(head){
+//             if(!binary_search(duplicates.begin(),duplicates.end(),head->val)){
+//                 ListNode * temp = new ListNode(head->val);
+//                 ans->next = temp;
+//                 ans=temp;
+//             }
+//             head=head->next;
+//         }
+//         return start->next;        
+//     }
+// };
+// ⤴️ 1st approach
+// \U0001f447⤵️ Second approach
 class Solution {
-private:  
-    void findDuplicates(ListNode * head , vector<int> & duplicates) {
-        if(!head) return;
-        while(head && head->next){
-            if(head->val==head->next->val){
-                duplicates.push_back(head->val);
-            }
-            head=head->next;
+private:
+    void insertList(ListNode * & ans , int val){
+        ListNode * temp = new ListNode(val);
+        if(!ans){
+            ans = temp;
+            return;
         }
-    }   
+        temp->next = ans;
+        ans = temp;
+        return;
+    }
 public:
     ListNode* deleteDuplicates(ListNode* head) {
-        vector<int> duplicates;
-        findDuplicates(head,duplicates);
-        ListNode * ans = new ListNode(0);
-        ListNode * start = ans;
-        while(head){
-            if(!binary_search(duplicates.begin(),duplicates.end(),head->val)){
-                ListNode * temp = new ListNode(head->val);
-                ans->next = temp;
-                ans=temp;
+        if(!head || !head->next) return head;
+        ListNode * curr = head;
+        stack<int>s;
+        int prev=0;
+        while(curr){
+            if(!s.empty() && s.top()==curr->val && curr){
+                while(s.top()==curr->val){
+                    curr=curr->next;
+                    if(!curr) break;
+                }
+                s.pop();
             }
-            head=head->next;
+            else{
+                s.push(curr->val);
+                curr=curr->next;
+            }
         }
-        return start->next;        
+        ListNode * ans = nullptr;
+        while(!s.empty()){
+            insertList(ans,s.top());
+            s.pop();
+        }
+        return ans;
     }
 };
