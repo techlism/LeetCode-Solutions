4f04e108dd1a1db5db7a0b77f4ee5f3473947063
techlism <105585386+techlism@users.noreply.github.com>
2023-05-20 08:24:29 +0530

    Time: 0 ms (100.00%), Space: 8.7 MB (84.10%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -5,22 +5,21 @@ public:
         int carry=0;
         vector<int>ans;
         for(int i=0;i<digits.size();i++){
+            int sum=0;
             if(i==0){
-                int sum=digits[i]+1;
-                ans.push_back(sum%10);
-                carry=sum/10;
+              sum=digits[i]+1;
+
             }
             else{
-                int sum=digits[i]+carry;
-                ans.push_back(sum%10);
-                carry=sum/10;
+                sum=digits[i]+carry;
             }
+            ans.insert(ans.begin(),sum%10);
+            carry=sum/10;
         }
         if(carry){
-            ans.push_back(carry);
+            ans.insert(ans.begin(),carry);
         }
-        reverse(ans.begin(),ans.end());
+        // reverse(ans.begin(),ans.end());
         return ans;
-        
     }
 };
