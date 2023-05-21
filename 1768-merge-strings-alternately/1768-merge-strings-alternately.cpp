b67042d080f86183efd112b34091648d03bf8901
techlism <105585386+techlism@users.noreply.github.com>
2023-05-21 08:52:31 +0530

    Time: 4 ms (41.17%), Space: 6.3 MB (58.14%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
+        
         string ans;
-        int l1=word1.length();
-        int l2=word2.length();
-        int i = 0;
-        int itr1=0;
-        int itr2=0;
+        int l1=word1.length(),l2=word2.length(),i=0,itr1=0,itr2=0;
+        
         while(itr1<l1 || itr2<l2){            
             if(i%2==0 && itr1 < l1){
                 // add first string
@@ -18,12 +16,15 @@ public:
             }          
             i++;
         }
+        
+        //check any leftover, if yes then append it at the last
         if(itr1!=l1-1){
             while(itr1<l1) ans.push_back(word1[itr1++]);
         }
         if(itr2!=l2-1){
             while(itr2<l2) ans.push_back(word2[itr2++]);
         }
+        
         return ans;
     }
 };
