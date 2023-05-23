78171c13c74575c79fb76c5aad59ec412bf53c91
techlism <105585386+techlism@users.noreply.github.com>
2023-05-23 10:01:43 +0530

    Time: 7 ms (66.32%), Space: 10.7 MB (18.83%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.cpp
+++ 0414-third-maximum-number/0414-third-maximum-number.cpp
@@ -2,19 +2,22 @@ class Solution {
 public:
     int thirdMax(vector<int>& nums) {
         set<int>s;
-        for(int i=0;i<nums.size();i++){
-            s.insert(nums[i]);
+        for(int num : nums){
+            s.insert(num);
         }
         if(s.size()<=3){
             if(s.size()==1 ||s.size()==3){
+                // if size==3 so the begining element will be the third maximum
                 return *s.begin();
             } 
             else{
+                // For Two elements the second value will be maximum
                 auto itr=s.begin();
                 return *(++itr);                
             }
         }
         else{
+            //Size is greater than 3.So, 3rd element from last will be the answer.
             auto itr=s.end();
             --itr;
             --itr;
