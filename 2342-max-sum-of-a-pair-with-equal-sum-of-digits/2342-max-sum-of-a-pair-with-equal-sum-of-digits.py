901a8dbbffd6a689af81129049c8f108c6e9195c
techlism <105585386+techlism@users.noreply.github.com>
2023-06-25 12:50:55 +0530

    Time: 1125 ms (54.16%), Space: 31.7 MB (31.69%) - LeetHub

--- 2342-max-sum-of-a-pair-with-equal-sum-of-digits/2342-max-sum-of-a-pair-with-equal-sum-of-digits.py
+++ 2342-max-sum-of-a-pair-with-equal-sum-of-digits/2342-max-sum-of-a-pair-with-equal-sum-of-digits.py
@@ -1,55 +1,23 @@
-class Solution {
-// private:
-//     int digitSum(int n){
-//         int sum = 0;
-//         while(n>0){
-//             sum=sum+(n%10);
-//             n=n/10;
-//         }
-//         return sum;
-//     }
-public:
-//     int maximumSum(vector<int>& nums) {
-//         unordered_multimap<int,int> map;
-//         int ans=-1;
-//         for(int i = 0;i<nums.size();i++){
-//             map.insert(make_pair(digitSum(nums[i]),i));
-//         }
-//         for(auto itr : map){
-//             auto range = map.equal_range(itr.first);
-
-//             for (auto it = range.first; it != range.second; ++it) {
-//                 if(itr.second!=it->second){
-//                     ans=max(ans,(nums[itr.second]+nums[it->second]));
-//                 }
-//             }            
-//         }
-//         return ans;
-//     }
-// \U0001f446 The approach that I could come up with    
-    int calculateSumOfDigits(int num) {
-        int sum = 0;
-        while (num > 0) {
-            sum += num % 10;
-            num /= 10;
-        }
-        return sum;
-    }
-
-    int maximumSum(vector<int>& nums) {
-        unordered_map<int, int> maxNumBySum;
-
-        int maxSum = -1;
-        for (int num : nums) {
-            int sum = calculateSumOfDigits(num);
-            if (maxNumBySum.count(sum) > 0) {
-                // that other number with same number of digits is present
-                maxSum = max(maxSum, num + maxNumBySum[sum]);
-            }
-            // Else create a key-value pair in the map
-            maxNumBySum[sum] = max(maxNumBySum[sum], num);
-        }
-
-        return maxSum;
-    }    
-};
+from typing import List
+class Solution:
+    @staticmethod
+    def digitSum(num:int):
+        sum=0
+        n=num
+        while(n>0):
+            sum = sum + n%10
+            n = n//10
+        return sum
+    
+    def maximumSum(self, nums: List[int]) -> int:
+        maxSum=-1
+        maxNumbyDigitSum = {}
+        for num in nums:
+            dsum = Solution.digitSum(num)
+            if(dsum in maxNumbyDigitSum):
+                maxSum=max(maxSum,num+maxNumbyDigitSum[dsum])
+            #for the map it accepts two value (a default to be returned if nothing found)
+            maxNumbyDigitSum[dsum]=max(num,maxNumbyDigitSum.get(dsum,0))
+        
+        return maxSum
+            
