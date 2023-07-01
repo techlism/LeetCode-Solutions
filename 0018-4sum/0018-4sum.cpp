6434695981444806af9dd9e36503603111bfb426
techlism <105585386+techlism@users.noreply.github.com>
2023-07-01 09:07:33 +0530

    Time: 124 ms (38.04%), Space: 13.1 MB (73.02%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -2,13 +2,14 @@ class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, long long target) {
         sort(nums.begin(), nums.end());
-        vector<vector<int>> ans;
-        
+        vector<vector<int>> ans;        
         for(int i = 0; i < nums.size(); i++) {
+            // Avoid Duplicates
             if (i > 0 && nums[i] == nums[i - 1])
                 continue;
             
             for(int j = i + 1; j < nums.size(); j++) {
+                // Avoid Duplicates
                 if (j > i + 1 && nums[j] == nums[j - 1])
                     continue;
                 
@@ -16,6 +17,7 @@ public:
                 int back = nums.size() - 1;
                 
                 while(front < back) {
+                    // To avoid Int Overflow
                     long long sum = static_cast<long long>(nums[front]) + nums[back]; // Perform addition with long long
                     if (sum > target && (sum - target) > INT_MAX)
                         break; // No need to continue searching in this direction
@@ -26,6 +28,7 @@ public:
                     
                     if (currentSum == target) {
                         ans.push_back({nums[i], nums[j], nums[front], nums[back]});
+                        // Avoiding duplicate Quads
                         while(front < back && nums[front] == nums[front + 1])
                             front++;
                         while(front < back && nums[back] == nums[back - 1])
