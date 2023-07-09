5c60f447880c1c3462d407b1c1dbba3ceee84daf
techlism <105585386+techlism@users.noreply.github.com>
2023-07-09 15:54:52 +0530

    Time: 60 ms (19.31%), Space: 28.9 MB (8.16%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string,set<int>>map;
+        unordered_map<string,unordered_set<int>>map;
         
         for(int i=0;i<strs.size();i++){
             string str = strs[i];
@@ -11,7 +11,7 @@ public:
         vector<vector<string>> ans;
         for(auto s:map){
             vector<string> grp;
-            for(auto i: s.second) grp.push_back(strs[i]);
+            for(auto i: s.second) grp.emplace_back(strs[i]);
             ans.emplace_back(grp);
         }
         return ans;
