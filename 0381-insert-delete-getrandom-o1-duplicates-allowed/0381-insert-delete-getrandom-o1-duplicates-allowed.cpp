d182a0a5ca8f833470cc1567b1821f3c8c6ab13e
techlism <105585386+techlism@users.noreply.github.com>
2023-07-10 08:12:29 +0530

    Time: 271 ms (59.29%), Space: 103.4 MB (86.49%) - LeetHub

--- 0381-insert-delete-getrandom-o1-duplicates-allowed/0381-insert-delete-getrandom-o1-duplicates-allowed.cpp
+++ 0381-insert-delete-getrandom-o1-duplicates-allowed/0381-insert-delete-getrandom-o1-duplicates-allowed.cpp
@@ -1,40 +1,83 @@
+// class RandomizedCollection {
+//     unordered_map<int, unordered_set<int>> map;
+//     vector<int>nums;
+// public:
+//     RandomizedCollection() {
+        
+//     }
+    
+//     bool insert(int val) {
+//         bool present=true;
+//         if(map.find(val) == map.end()) present=false;
+//         nums.push_back(val);
+//         map[val].insert(nums.size()-1);
+//         return !present;
+//     }
+    
+//     bool remove(int val) {
+//     // not present
+//     if(map.find(val)==map.end() || map[val].empty() ){
+//         return false;
+//     }
+//     else{           
+//         auto first = map[val].begin();
+//         int firstindex = *(first);
+//         map[val].erase(firstindex);
+//         if (map[val].empty()) {
+//             map.erase(val);
+//         }
+//         if (firstindex != nums.size() - 1) {
+//             nums[firstindex]=nums.back(); // swapping the values
+//             int back = nums.back();
+//             map[back].erase(nums.size()-1);
+//             map[back].insert(firstindex);
+//         }
+//         nums.pop_back();
+//     }
+//     return true;
+//     }
+    
+//     int getRandom() {
+//         return nums[rand() % nums.size()];
+//     }
+// }; --> First Solution
+
 class RandomizedCollection {
-    unordered_map<int, unordered_set<int>> map;
-    vector<int>nums;
+    unordered_map<int, vector<int>> map;
+    vector<int> nums;
 public:
     RandomizedCollection() {
         
     }
     
     bool insert(int val) {
-        bool present=true;
-        if(map.find(val) == map.end()) present=false;
+        bool present = true;
+        if (map.find(val) == map.end()) present = false;
         nums.push_back(val);
-        map[val].insert(nums.size()-1);
+        map[val].push_back(nums.size() - 1);
         return !present;
     }
     
     bool remove(int val) {
-    // not present
-    if(map.find(val)==map.end() || map[val].empty() ){
-        return false;
-    }
-    else{           
-        auto first = map[val].begin();
-        int firstindex = *(first);
-        map[val].erase(firstindex);
-        if (map[val].empty()) {
-            map.erase(val);
+        // not present
+        if (map.find(val) == map.end() || map[val].empty()) {
+            return false;
         }
-        if (firstindex != nums.size() - 1) {
-            nums[firstindex]=nums.back(); // swapping the values
-            int back = nums.back();
-            map[back].erase(nums.size()-1);
-            map[back].insert(firstindex);
+        else {
+            int firstindex = map[val].back();
+            map[val].pop_back();
+            if (map[val].empty()) {
+                map.erase(val);
+            }
+            if (firstindex != nums.size() - 1) {
+                nums[firstindex] = nums.back(); // swapping the values
+                int back = nums.back();
+                auto it = find(map[back].begin(), map[back].end(), nums.size() - 1);
+                *it = firstindex;
+            }
+            nums.pop_back();
         }
-        nums.pop_back();
-    }
-    return true;
+        return true;
     }
     
     int getRandom() {
