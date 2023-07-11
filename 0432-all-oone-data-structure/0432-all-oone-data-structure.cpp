aceb2656cb786beaebc12eebb1e5326d0f86b0a5
techlism <105585386+techlism@users.noreply.github.com>
2023-07-11 05:37:45 +0530

    Time: 144 ms (46.06%), Space: 56.1 MB (33.03%) - LeetHub

---
+++ 0432-all-oone-data-structure/0432-all-oone-data-structure.cpp
@@ -0,0 +1,44 @@
+class AllOne {
+private:
+    unordered_map<string, int> stf;
+    map<int , unordered_set<string>> fts;
+public:
+    AllOne() {}
+
+    void inc(string key) {
+        if(stf[key] != 0){
+            fts[stf[key]].erase(key);
+            if (fts[stf[key]].empty()) {
+                fts.erase(stf[key]);
+            }
+        }
+        ++stf[key];
+        fts[stf[key]].insert(key);
+    }
+
+    void dec(string key) {
+        if (stf.find(key) == stf.end()) return;
+
+        fts[stf[key]].erase(key);
+        if (fts[stf[key]].empty()) {
+            fts.erase(stf[key]);
+        }
+
+        --stf[key];
+        if (stf[key] == 0) {
+            stf.erase(key);
+        } else {
+            fts[stf[key]].insert(key);
+        }
+    }
+
+    string getMaxKey() {
+        if(fts.empty()) return "";
+        else return *(fts.rbegin()->second.begin());
+    }
+
+    string getMinKey() {
+        if(fts.empty()) return "";
+        else return *(fts.begin()->second.begin());
+    }
+};
