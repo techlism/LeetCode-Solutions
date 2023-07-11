75fc7ea8a82386664c7e1995716bb19d26e60f8c
techlism <105585386+techlism@users.noreply.github.com>
2023-07-11 19:50:15 +0530

    Time: 15 ms (39.26%), Space: 8.6 MB (27.59%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,8 +1,10 @@
 class Solution {
+// Time Complexity : O(NLOGN)
+// Space Complexity : O(N) + O(N) = O(N)
 public:
     string frequencySort(string s) {
         unordered_map <char,int> ctof;
-        map<int, vector<char> >fts;
+        map<int, string,greater<int> >fts;
         string ans="";
         for(auto ch : s) ctof[ch]+=1;
         for(auto el : ctof) fts[el.second].push_back(el.first);
@@ -16,7 +18,6 @@ public:
                 }               
             }
         }
-        reverse(ans.begin(),ans.end());
         return ans;
     }
 };
