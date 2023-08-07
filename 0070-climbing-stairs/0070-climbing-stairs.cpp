a9e7f12d07eaf01218aea6af296c4c92cc7ccf1b
techlism <105585386+techlism@users.noreply.github.com>
2023-08-08 00:12:38 +0530

    Time: 3 ms (30.87%), Space: 6.3 MB (13.64%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -3,6 +3,7 @@ private:
     int solve(int n, vector<int> & dp){
         
         if( n == 0 || n == 1) return 1;
+        if( n == 2) return 2;
         if(dp[n]!=-1) return dp[n];
         dp[n] = solve(n-1,dp) + solve(n-2,dp);
         return dp[n];
