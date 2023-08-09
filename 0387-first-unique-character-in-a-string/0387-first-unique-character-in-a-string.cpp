3a3889e6284b3574135f888141abe2ad765104cd
techlism <105585386+techlism@users.noreply.github.com>
2023-08-09 17:35:33 +0530

    Time: 38 ms (58.87%), Space: 10.9 MB (9.69%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
-        unordered_map< char, pair< int,set<int> > > map;
+        unordered_map< char, pair< int,int > > map;
+        
         for(int i = 0;i<s.length();i++){
-            map[s[i]].first++;
-            map[s[i]].second.insert(i);
+            if (map.find(s[i]) == map.end()) {
+                map[s[i]] = {1, i};
+            } else {
+                map[s[i]].first++;
+            }
         }
         int ans = INT_MAX ;
-        for(auto itr : map){
-            if(itr.second.first==1){
-                ans=min(ans,*itr.second.second.begin());
-            } 
-        }
+        for(auto itr : map) 
+            if(itr.second.first==1) 
+                ans = min(ans, itr.second.second);
+        
         return ans == INT_MAX ? -1 : ans ;
     }
 };
