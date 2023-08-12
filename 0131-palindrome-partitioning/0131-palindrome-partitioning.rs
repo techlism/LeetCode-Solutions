20a052c9bb59bab9e48e442335e83a419bc8193c
techlism <105585386+techlism@users.noreply.github.com>
2023-08-12 09:04:37 +0530

    Time: 75 ms (100.00%), Space: 20.8 MB (32.35%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.rs
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.rs
@@ -8,17 +8,16 @@ fn palindrome(chars: &Vec<char>, mut start: usize, mut end: usize) -> bool {
     }
     return true;
 }
-fn create_partition(s:&String , ans : &mut Vec<Vec<String>>, 
+fn create_partition(chars: &Vec<char>, ans : &mut Vec<Vec<String>>, 
     path: &mut Vec<String>, index : usize ){
-        if(index>=s.len()){
+        if(index>=chars.len()){
             ans.push(path.clone());
             return;
-        }
-        let chars : Vec<char> = s.chars().collect();
-        for i in index..s.len() {
+        }        
+        for i in index..chars.len() {
             if palindrome(&chars,index,i){
                 path.push(chars[index..=i].iter().collect());
-                create_partition(s,ans,path,i+1);
+                create_partition(chars,ans,path,i+1);
                 path.pop();
             }
         }        
@@ -27,7 +26,8 @@ impl Solution {
     pub fn partition(s: String) -> Vec<Vec<String>> {
         let mut path : Vec<String> = Vec::new();
         let mut ans : Vec<Vec<String>> = Vec::new();
-        create_partition(&s,&mut ans, &mut path,0);
+        let chars : Vec<char> = s.chars().collect();
+        create_partition(&chars,&mut ans, &mut path,0);
         return ans;
     }
 }
