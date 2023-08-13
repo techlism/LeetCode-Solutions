5d90a2e1f3873858437ed84a73c2fd4602e94dab
techlism <105585386+techlism@users.noreply.github.com>
2023-08-13 20:27:04 +0530

    Time: 1 ms (84.17%), Space: 2.2 MB (40.54%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.rs
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.rs
@@ -3,9 +3,8 @@ impl Solution {
     pub fn maximum_wealth(accounts: Vec<Vec<i32>>) -> i32 {
         let mut ans = 0;
         for i in 0..accounts.len() {
-            let wealth = accounts[i].iter().sum();
-            ans = cmp::max(ans,wealth);            
+            ans = cmp::max(ans,accounts[i].iter().sum());            
         }
-        ans
+        return ans;
     }
 }
