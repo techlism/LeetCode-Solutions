c119973e2d6635d36bcb1c3e3d64869713bc18a0
techlism <105585386+techlism@users.noreply.github.com>
2023-08-13 20:13:36 +0530

    Time: 0 ms (100.00%), Space: 2 MB (74.51%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.rs
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.rs
@@ -1,26 +1,42 @@
 impl Solution {
     pub fn judge_circle(moves: String) -> bool {
-        let mut right = 0 ;
-        let mut left = 0 ;
-        let mut top =  0 ;
-        let mut down = 0 ;
+        // let mut right = 0 ;
+        // let mut left = 0 ;
+        // let mut top =  0 ;
+        // let mut down = 0 ;
+        // for mov in moves.chars() {
+        //     if mov== 'R'{
+        //         right+=1;
+        //     }
+        //     if mov=='L' {
+        //         left+=1;
+        //     }
+        //     if mov=='U' {
+        //         top+=1;
+        //     }
+        //     if mov=='D' {
+        //         down+=1;
+        //     }
+        // }
+        // if (right==left) && (top==down) {
+        //     return true;
+        // }
+        // return false;
+        // My Solution \U0001f446. A bit optimized one \U0001f447
+        let mut horizontal = 0;
+        let mut vertical = 0;
+
         for mov in moves.chars() {
-            if mov== 'R'{
-                right+=1;
-            }
-            if mov=='L' {
-                left+=1;
-            }
-            if mov=='U' {
-                top+=1;
-            }
-            if mov=='D' {
-                down+=1;
+            match mov {
+                'R' => horizontal += 1,
+                'L' => horizontal -= 1,
+                'U' => vertical += 1,
+                'D' => vertical -= 1,
+                // if none of the above condition matches
+                _ => (),
             }
         }
-        if (right==left) && (top==down) {
-            return true;
-        }
-        return false;
+
+        horizontal == 0 && vertical == 0        
     }
 }
