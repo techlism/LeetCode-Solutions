7310c38fd533e7af08d16d70208e9937f480d4a6
techlism <105585386+techlism@users.noreply.github.com>
2023-08-13 09:38:06 +0530

    Time: 0 ms (100.00%), Space: 2.2 MB (43.40%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.rs
@@ -0,0 +1,13 @@
+impl Solution {
+    pub fn find_the_difference(s: String, t: String) -> char {
+        let mut sum1: u8 = 0;
+        let mut sum2: u8 = 0;
+        for i in s.chars() {
+            sum1+= i as u8;
+        }
+        for i in t.chars() {
+            sum2+= i as u8;
+        }
+        return (sum2 - sum1) as char ;
+    }
+}
