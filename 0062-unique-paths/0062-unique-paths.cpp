f78b3bc99d4301c4e73edd875d75cd8c2b9c7314
techlism <105585386+techlism@users.noreply.github.com>
2023-08-25 19:08:25 +0530

    Time: 3 ms (34.24%), Space: 6.5 MB (22.86%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,21 +1,36 @@
 class Solution {
-private:
-    int countPaths(vector<vector<int>> &dp,int m,int n){
-        if(m==0 && n==0){
-            return 1;
-        }
-        if(m < 0 || n < 0){
-            return 0;
-        }
-        if(dp[m][n] != -1) return dp[m][n];
-        int up = countPaths(dp,m-1,n);
-        int left = countPaths(dp,m,n-1);
-        return dp[m][n] = up + left;
-    }
+// private:
+//     // Memoization
+//     int countPaths(vector<vector<int>> &dp,int m,int n){
+//         if(m==0 && n==0){
+//             return 1;
+//         }
+//         if(m < 0 || n < 0){
+//             return 0;
+//         }
+//         if(dp[m][n] != -1) return dp[m][n];
+//         int up = countPaths(dp,m-1,n);
+//         int left = countPaths(dp,m,n-1);
+//         return dp[m][n] = up + left;
+//     }
 public:
     int uniquePaths(int m, int n) {
         vector<vector<int>> dp (m,vector<int>(n,-1));
-        return countPaths(dp,m-1,n-1);
-        
+        //Tabulation
+        dp[0][0]=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0){
+                    dp[i][j]=1;
+                    continue;
+                }
+                int up = 0;
+                int left = 0;
+                if (i>0) up = dp[i-1][j];
+                if (j>0) left = dp[i][j-1];
+                dp[i][j]=up+left;
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
