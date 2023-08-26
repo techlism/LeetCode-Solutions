46286afad0e233243c9f83372e83aeca7d4a1964
techlism <105585386+techlism@users.noreply.github.com>
2023-08-26 09:45:40 +0530

    Time: 12 ms (35.55%), Space: 10.1 MB (16.93%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,5 +1,6 @@
 class Solution {
 private:
+    // memoization
     int minPath(vector<vector<int>> &grid, vector<vector<int>> &dp ,int i,int j){
         if( i==0 && j==0 ) return grid[0][0] ;
         if (i < 0 || j < 0 ) return 1e9;
@@ -11,6 +12,23 @@ public:
         int m = grid.size();
         int n = grid[0].size();
         vector<vector<int>> dp (m,vector<int>(n,-1));
-        return minPath(grid,dp,m-1,n-1);
+        // return minPath(grid,dp,m-1,n-1);
+        // Tabulation \U0001f447
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0){
+                    dp[i][j]=grid[i][j];
+                    continue;
+                }
+                int up = grid[i][j];
+                if(i>0) up+=dp[i-1][j];
+                else up+=1e9;
+                int left = grid[i][j];
+                if(j>0) left+=dp[i][j-1];
+                else left+=1e9;
+                dp[i][j]=min(left,up);
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
