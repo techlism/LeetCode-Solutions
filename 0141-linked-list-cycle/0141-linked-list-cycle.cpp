a9d986ce246927cc107f93c7599e2f2cdc6b7e83
techlism <105585386+techlism@users.noreply.github.com>
2023-09-04 08:00:40 +0530

    Time: 11 ms (49.91%), Space: 8.2 MB (36.51%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -11,12 +11,12 @@ public:
     bool hasCycle(ListNode *head) {
          ListNode *slowPointer = head;
          ListNode *fastPointer = head;
-    while(slowPointer != NULL && fastPointer != NULL&& fastPointer->next != NULL) {
-            slowPointer = slowPointer->next;
-            fastPointer = fastPointer->next->next;
-            if (slowPointer == fastPointer)
-                return 1;
+         while(slowPointer != NULL && fastPointer != NULL&& fastPointer->next != NULL) {
+                slowPointer = slowPointer->next;
+                fastPointer = fastPointer->next->next;
+                if (slowPointer == fastPointer)
+                    return 1;
+         }
+        return 0;
         }
-    return 0;
-    }
 };
