40cb18371366496da550c59eb454ecd46d634504
techlism <105585386+techlism@users.noreply.github.com>
2023-09-06 12:20:58 +0530

    Time: 1057 ms (11.27%), Space: 298.5 MB (5.00%) - LeetHub

---
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -0,0 +1,31 @@
+class Solution {
+private:
+    void prepareAdj(unordered_map<int,list<int>> &adjList,vector<vector<int>>&edges){
+        for(auto edge : edges){
+            adjList[edge[0]].push_back(edge[1]);
+            adjList[edge[1]].push_back(edge[0]);
+        }
+        return;
+    }
+    void dfs(int n,unordered_map<int,list<int>>&adjList,vector<bool>&visited,
+             set<int> &curr){
+        visited[n]=true;
+        curr.insert(n);
+        for(auto node : adjList[n]){
+            if(!visited[node]) dfs(node,adjList,visited,curr);
+        }
+    }
+public:
+    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
+        unordered_map<int,list<int>> adjList;
+        prepareAdj(adjList,edges);
+        vector<bool> visited(n,false);
+        for(int i=0;i<n;i++){
+            set<int> curr;
+            if(!visited[i]) dfs(i,adjList,visited,curr);
+            if(curr.find(source) != curr.end() && curr.find(destination)!= curr.end())
+                return true;
+        }
+        return false;
+    }
+};
