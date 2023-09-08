237550b327bc003f5b3825facf2a398cc8ba9197
techlism <105585386+techlism@users.noreply.github.com>
2023-09-08 23:49:45 +0530

    Time: 484 ms (26.91%), Space: 106.6 MB (67.37%) - LeetHub

--- 2442-count-number-of-distinct-integers-after-reverse-operations/2442-count-number-of-distinct-integers-after-reverse-operations.cpp
+++ 2442-count-number-of-distinct-integers-after-reverse-operations/2442-count-number-of-distinct-integers-after-reverse-operations.cpp
@@ -9,8 +9,13 @@ public:
     int countDistinctIntegers(vector<int>& nums) {
         unordered_set<int> unique;
         for(auto num : nums) {
-            unique.insert(num);
-            unique.insert(intRev(num));
+            int rev = intRev(num);
+            if(rev != num){
+                unique.insert(num);
+                unique.insert(intRev(num));                
+            }
+            else unique.insert(num);
+
         }
         return unique.size();
     }
