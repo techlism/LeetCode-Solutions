2080f8411920d6ee5ac6823b36e5b4a94a86e2f4
techlism <105585386+techlism@users.noreply.github.com>
2023-09-08 16:54:34 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (20.86%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> ans(numRows);
+        if(numRows >=1){
+            ans[0].push_back(1);
+            if(numRows >= 2) {
+                ans[1].push_back(1);
+                ans[1].push_back(1);
+            }
+        }
+        for(int i = 2;i<numRows;i++){
+            ans[i].push_back(1);
+            for(int j = 1;j<i;j++){
+                ans[i].push_back(ans[i-1][j]+ans[i-1][j-1]);
+            }
+            ans[i].push_back(1);
+        }
+        return ans;
+    }
+};
