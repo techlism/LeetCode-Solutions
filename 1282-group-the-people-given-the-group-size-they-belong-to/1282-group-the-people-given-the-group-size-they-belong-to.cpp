fddf02e5e55817eb87ed3c26b0c1e5c98b393b4b
techlism <105585386+techlism@users.noreply.github.com>
2023-09-11 10:27:03 +0530

    Time: 12 ms (50.27%), Space: 12.8 MB (76.51%) - LeetHub

--- 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
@@ -1,25 +1,37 @@
 class Solution {
 public:
     vector<vector<int>> groupThePeople(vector<int>& groupSizes) {
-        unordered_map<int,vector<int>>sizeToPerson;
-        for(int i=0;i<groupSizes.size();i++){
-            sizeToPerson[groupSizes[i]].push_back(i);
-        }
+        // unordered_map<int,vector<int>>sizeToPerson;
+        // for(int i=0;i<groupSizes.size();i++){
+        //     sizeToPerson[groupSizes[i]].push_back(i);
+        // }
+        // vector<vector<int>> ans;
+        // for(auto itr : sizeToPerson){
+        //     if(itr.first == itr.second.size()) ans.emplace_back(itr.second);
+        //     else if(itr.first < itr.second.size()){
+        //         int s = 0;
+        //         int e = itr.first;
+        //         while(e <= itr.second.size()){
+        //             vector<int> temp;
+        //             for(int i=s;i<e;i++) temp.emplace_back(itr.second[i]);
+        //             ans.emplace_back(temp);
+        //             s=e;
+        //             e+=itr.first;
+        //         }
+        //     }
+        // }
+        // return ans;
         vector<vector<int>> ans;
-        for(auto itr : sizeToPerson){
-            if(itr.first == itr.second.size()) ans.emplace_back(itr.second);
-            else if(itr.first < itr.second.size()){
-                int s = 0;
-                int e = itr.first;
-                while(e <= itr.second.size()){
-                    vector<int> temp;
-                    for(int i=s;i<e;i++) temp.emplace_back(itr.second[i]);
-                    ans.emplace_back(temp);
-                    s=e;
-                    e+=itr.first;
-                }
+        unordered_map<int, vector<int>> groups;
+        
+        for (int i = 0; i < groupSizes.size(); ++i) {
+            groups[groupSizes[i]].push_back(i);
+            if (groups[groupSizes[i]].size() == groupSizes[i]) {
+                ans.push_back({});
+                swap(ans.back(), groups[groupSizes[i]]);
             }
         }
-        return ans;
+        
+        return ans;        
     }
 }; 
