a4944185b450dd4564dbd327fb19fd02f745cc1e
techlism <105585386+techlism@users.noreply.github.com>
2023-12-05 06:48:49 +0530

    Time: 2 ms (44.05%), Space: 6.3 MB (73.09%) - LeetHub

---
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int numberOfMatches(int n) {
+        if (n == 1) {
+            return 0;
+        }
+        if (n % 2 == 0) {
+            return n / 2 + numberOfMatches(n / 2);
+        } else {
+            return n / 2 + numberOfMatches(n / 2 + 1);
+        }
+    }
+};
