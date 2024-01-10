03fd4bb1f2e666dd86df65454180c7be26419169
techlism <105585386+techlism@users.noreply.github.com>
2024-01-10 07:37:19 +0530

    Time: 0 ms (100.00%), Space: 13.4 MB (30.46%) - LeetHub

--- 0872-leaf-similar-trees/0872-leaf-similar-trees.cpp
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.cpp
@@ -11,31 +11,22 @@
  */
 class Solution {
 private:
-    void post(TreeNode * root1, vector<int> & leaf){
-        if(root1==nullptr){
-            return;
-        }
-        post(root1->left,leaf);
-        post(root1->right,leaf);
-        if(root1->left==nullptr && root1->right==nullptr){
-            leaf.push_back(root1->val);
-        }
+    void dfs(TreeNode* root,vector<int>& leaf){
+        if(!root) return;
+        dfs(root->left,leaf);
+        if(!root->left && !root->right) leaf.push_back(root->val);
+        dfs(root->right,leaf);
     }
 public:
     bool leafSimilar(TreeNode* root1, TreeNode* root2) {
-        vector<int>leaf1;
-        vector<int>leaf2;
-        post(root1,leaf1);
-        post(root2,leaf2);
-        if(leaf1.size()!=leaf2.size()){
-            return false;
-        }
-        for(int i=0;i<leaf1.size();i++){
-            if(leaf1[i]!=leaf2[i]){
-                return false;
-            }
+        vector<int> leaf1;
+        vector<int> leaf2;
+        dfs(root1,leaf1);
+        dfs(root2,leaf2);
+        if(leaf1.size() != leaf2.size()) return false;
+        for(int i = 0;i<leaf1.size();i++){
+            if(leaf1[i] != leaf2[i]) return false;
         }
         return true;
-        
     }
 };
