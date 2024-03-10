6ff8d6f962b539bee5942769bfe059566ceaa37d
techlism <105585386+techlism@users.noreply.github.com>
2024-03-10 19:51:06 +0530

    Time: 5 ms (49.43%), Space: 14.4 MB (7.77%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -1,36 +1,18 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        sort(nums1.begin(), nums1.end()); // nlogn
-        sort(nums2.begin(), nums2.end()); // nlogn
-        
-        int i = 0,j = 0;
-        int m = nums1.size();
-        int n = nums2.size();
-        
-        vector<int> res;
-        
-        while(i<m && j<n){
-            if(i>0 &&  nums1[i-1] == nums1[i]){
-                /*if we have already checked and traversed the nums[i-1] before and its same as previous.
-                So we can skip this:*/
-                //Most important thing that I was missing out
-                i++;
-                continue;
-            }
-            if(nums1[i] < nums2[j]){
-                 i++;   
-            }
-            else if(nums1[i] > nums2[j]){
-                j++;
-            }
-            else{ 
-            // If we found intersecting/common elements(Our Answer)
-                res.push_back(nums1[i]);
-                i++;
-                j++;
-            }
+        unordered_map<int,string> inter;
+        for(auto num : nums1){
+            inter[num].push_back('a');
         }
-        return res;
+        for(auto num : nums2){
+            inter[num].push_back('b');
+        }
+        vector<int> ans;
+        for(auto itr : inter){
+            if(itr.second.find('a') != string::npos && itr.second.find('b') != string::npos)
+                ans.emplace_back(itr.first);
+        }
+        return ans;
     }
 };
