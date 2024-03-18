a9834dc13635604037b1e2acc8c12762f476e9c6
techlism <105585386+techlism@users.noreply.github.com>
2024-03-18 10:14:53 +0530

    Time: 36 ms (99.54%), Space: 48.7 MB (76.72%) - LeetHub

--- 2634-filter-elements-from-array/2634-filter-elements-from-array.js
+++ 2634-filter-elements-from-array/2634-filter-elements-from-array.js
@@ -5,8 +5,10 @@
  */
 var filter = function(arr, fn) {
     let ans  = [];
-    arr.map((el, index)=>{
-        if(fn(el,index)) ans.push(el);
-    })
+    
+    for(let i=0;i<arr.length;i++){
+        if(fn(arr[i],i)) ans.push(arr[i]);
+    }
+    
     return ans
 };
