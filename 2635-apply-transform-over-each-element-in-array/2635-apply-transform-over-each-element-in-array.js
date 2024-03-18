84f0d98c0581b28b93fc9cf3d893e18df3984dd2
techlism <105585386+techlism@users.noreply.github.com>
2024-03-18 10:19:00 +0530

    Time: 50 ms (66.07%), Space: 48.8 MB (58.51%) - LeetHub

--- 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
+++ 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
@@ -4,10 +4,11 @@
  * @return {number[]}
  */
 var map = function(arr, fn) {
-    let ans = [];
-    for(let i=0;i<arr.length;i++){
-        let output = fn(arr[i],i);
-        ans.push(output);
-    }
-    return ans;
+    // let ans = [];
+    // for(let i=0;i<arr.length;i++){
+    //     let output = fn(arr[i],i);
+    //     ans.push(output);
+    // }
+    // return ans;
+    return arr.map((element,index)=>fn(element,index));
 };
