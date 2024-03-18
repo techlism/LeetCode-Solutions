a2cba53f8fbfba213b40711bfd924b44631df439
techlism <105585386+techlism@users.noreply.github.com>
2024-03-18 10:59:55 +0530

    Time: 56 ms (33.06%), Space: 48.7 MB (68.27%) - LeetHub

---
+++ 2666-allow-one-function-call/2666-allow-one-function-call.js
@@ -0,0 +1,22 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var once = function(fn) {
+    let called = false;
+    return function(...args){
+        if(!called){
+            called = true;
+            return fn(...args);            
+        }     
+        else return undefined; 
+    }
+};
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
