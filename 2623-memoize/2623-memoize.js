cb2acb2da83e1318dc361fb9d425e3ab7596dd88
techlism <105585386+techlism@users.noreply.github.com>
2024-03-18 10:44:37 +0530

    Time: 309 ms (43.17%), Space: 89 MB (59.29%) - LeetHub

---
+++ 2623-memoize/2623-memoize.js
@@ -0,0 +1,32 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+function memoize(fn) {
+   const cache = {};
+  
+   return function(...args) {
+        const key = JSON.stringify(args);
+
+        if (key in cache) {
+          return cache[key];
+        }
+
+        const result = fn.apply(this, args);
+        cache[key] = result;
+
+        return result;
+   }
+}
+
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
