5f04381bbf54796ca39f6e0078ea09ff4c843125
techlism <105585386+techlism@users.noreply.github.com>
2024-04-23 00:21:02 +0530

    Time: 56 ms (91.56%), Space: 49.8 MB (7.22%) - LeetHub

---
+++ 2715-timeout-cancellation/2715-timeout-cancellation.js
@@ -0,0 +1,41 @@
+/**
+ * @param {Function} fn
+ * @param {Array} args
+ * @param {number} t
+ * @return {Function}
+ */
+const cancellable = function(fn, args, t) {
+      let isCancelled = false;
+      setTimeout(() => {
+          if(!isCancelled)
+              fn(...args);
+      }, t);
+
+      return () => {
+        isCancelled = true;
+      };
+};
+
+/**
+ *  const result = [];
+ *
+ *  const fn = (x) => x * 5;
+ *  const args = [2], t = 20, cancelTimeMs = 50;
+ *
+ *  const start = performance.now();
+ *
+ *  const log = (...argsArr) => {
+ *      const diff = Math.floor(performance.now() - start);
+ *      result.push({"time": diff, "returned": fn(...argsArr)});
+ *  }
+ *       
+ *  const cancel = cancellable(log, args, t);
+ *
+ *  const maxT = Math.max(t, cancelTimeMs);
+ *           
+ *  setTimeout(cancel, cancelTimeMs);
+ *
+ *  setTimeout(() => {
+ *      console.log(result); // [{"time":20,"returned":10}]
+ *  }, maxT + 15)
+ */
