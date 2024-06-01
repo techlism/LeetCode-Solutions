60a7c4218bc9548c14016107ccb1831b16f69c97
techlism <105585386+techlism@users.noreply.github.com>
2024-06-01 12:06:36 +0530

    Time: 5 ms (27.27%), Space: 7.9 MB (42.17%) - LeetHub

--- 3110-score-of-a-string/3110-score-of-a-string.cpp
+++ 3110-score-of-a-string/3110-score-of-a-string.cpp
@@ -3,9 +3,7 @@ public:
     int scoreOfString(string s) {
         int ans = 0;
         for(int i=0;i<s.size() - 1 ; i++){
-            int ascii1 = int(s[i]);
-            int ascii2 = int(s[i+1]);
-            ans += abs(ascii1 - ascii2);
+            ans += abs(int(s[i]) - int(s[i+1]));
         }
         return ans;
     }
