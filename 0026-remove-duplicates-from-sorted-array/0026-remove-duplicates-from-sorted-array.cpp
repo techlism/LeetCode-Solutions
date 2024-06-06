6a5c54d0fadb24a0e20fba22fd864ee6375b3302
techlism <105585386+techlism@users.noreply.github.com>
2024-06-06 17:25:36 +0530

    Time: 11 ms (35.07%), Space: 21.1 MB (64.58%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,22 +1,29 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        stack<int>stack;
-        for(int num : nums){
-            if(stack.empty()){
-                stack.push(num);  
-                continue;
-            } 
-            if(stack.top() != num){
-                stack.push(num);
-            }
-        }
-        int size = stack.size() - 1;
-        int ans = stack.size();
-        while(size >= 0){
-            nums[size--] = stack.top();
-            stack.pop();
-        }
-        return ans;
+        // stack<int>stack;
+        // for(int num : nums){
+        //     if(stack.empty()){
+        //         stack.push(num);  
+        //         continue;
+        //     } 
+        //     if(stack.top() != num){
+        //         stack.push(num);
+        //     }
+        // }
+        // int size = stack.size() - 1;
+        // int ans = stack.size();
+        // while(size >= 0){
+        //     nums[size--] = stack.top();
+        //     stack.pop();
+        // }
+    // return ans; // A good and intuitive solution but not memory efficient
+        int k=1;
+        for(int i=1; i<nums.size(); i++){
+         if(nums[i]!=nums[i-1]){
+           nums[k++] = nums[i];  
+         } 
+        }   
+        return k;
     }
 };
