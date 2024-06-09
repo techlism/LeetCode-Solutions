aba03a4a9ae193b6681a9a1816b6ab9109d95042
techlism <105585386+techlism@users.noreply.github.com>
2024-06-09 13:54:20 +0530

    Time: 36 ms (74.52%), Space: 32.5 MB (91.85%) - LeetHub

--- 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.cpp
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.cpp
@@ -5,6 +5,7 @@ public:
         count[0] = 1;
         int prefix = 0, res = 0;
         for (int a : nums) {
+            // this is the shit that you have to care about
             prefix = (prefix + a % k + k) % k;
             res += count[prefix]++;
         }
