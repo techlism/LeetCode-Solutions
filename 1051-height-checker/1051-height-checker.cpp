ed475372b733a3248e9fc572b8d9372a468ca9c4
techlism <105585386+techlism@users.noreply.github.com>
2024-06-10 12:57:07 +0530

    Time: 0 ms (100.00%), Space: 10.3 MB (10.73%) - LeetHub

--- 1051-height-checker/1051-height-checker.cpp
+++ 1051-height-checker/1051-height-checker.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     int heightChecker(vector<int>& heights) {
-        vector<int>expected=heights;
-        sort(expected.begin(),expected.end());
-        int ans=0;
-        for(int i=0;i<heights.size();i++){
-            if(heights[i]!=expected[i]){
-                ans++;
-            }
+        vector<int>sorted(heights.begin(),heights.end());
+        sort(sorted.begin(),sorted.end());
+        int count = 0;
+        for(int i = 0; i < heights.size(); i++){
+            if(heights[i] != sorted[i]) count += 1;
         }
-        return  ans;        
+        return count;
     }
 };
