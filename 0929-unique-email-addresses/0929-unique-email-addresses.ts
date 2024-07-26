ad21342b439b4cfc3a76d2e4dab439ecaf1ae60c
techlism <105585386+techlism@users.noreply.github.com>
2024-07-26 11:56:29 +0530

    Time: 73 ms (59.87%), Space: 54.8 MB (66.24%) - LeetHub

--- 0929-unique-email-addresses/0929-unique-email-addresses.ts
+++ 0929-unique-email-addresses/0929-unique-email-addresses.ts
@@ -2,10 +2,8 @@ function numUniqueEmails(emails: string[]): number {
     const unique = new Set();
     for(let email of emails){
         let divided = email.split('@');
-        let local : string | string[] = divided[0].split('+');
-        local = local[0];
-        local = local.split('.').join('');
-        let final = local+'@'+divided[1];
+        let local : string | string[] = divided[0].split('+');        
+        let final = local[0].split('.').join('')+'@'+divided[1];
         unique.add(final);
     }
     return unique.size;
